8bdbc44b2915b5d9b9d536f9a57af0977d00f288
Igor Volodin <postmaster@myghost.name>
2023-06-04 23:27:36 +0300

    Circular buffer lib

--- f4/dev_lib/cbuf.c
+++ f4/dev_lib/cbuf.c
@@ -65,3 +65,55 @@ int8_t buf_pop_s16(cbuf_s16 *c, int16_t *data){
     if (c ->head >= c->tail) { c->length = (c->head - c->tail); } else { c->length = c->maxindex - c->tail + c->head + 1;}
     return 0;
 }
+
+// New realization
+
+uint8_t rb_u8_push(rb_u8 *ring, uint8_t data)
+{
+  if (((ring->tail + 1) % ring->size) != ring->head)
+  {
+    ring->data[ring->tail++] = data;
+    ring->tail %= ring->size;
+    ring->length++;
+    return 1;
+  }
+  return 0;
+}
+
+uint8_t rb_u16_push(rb_u16 *ring, uint16_t data)
+{
+  if (((ring->tail + 1) % ring->size) != ring->head)
+  {
+    ring->data[ring->tail++] = data;
+    ring->tail %= ring->size;
+    ring->length++;
+    return 1;
+  }
+  return 0;
+}
+
+uint8_t rb_u8_pop(rb_u8 *ring, uint8_t *data)
+{
+  if (ring->head != ring->tail)
+  {
+    *data = ring->data[ring->head++];
+    ring->head %= ring->size;
+    ring->length--;
+    return 1;
+  }
+  return 0;
+}
+
+uint8_t rb_u16_pop(rb_u16 *ring, uint16_t *data)
+{
+  if (ring->head != ring->tail)
+  {
+    *data = ring->data[ring->head++];
+    ring->head %= ring->size;
+    ring->length--;
+    return 1;
+  }
+  return 0;
+}
+
+//
--- f4/dev_lib/cbuf.h
+++ f4/dev_lib/cbuf.h
@@ -26,6 +26,7 @@ typedef struct {
 } cbuf_u8;
 
 
+
 int8_t buf_push_u16(cbuf_u16 *c, uint16_t data);
 int8_t buf_pop_u16(cbuf_u16 *c, uint16_t *data);
 int8_t buf_push_s16(cbuf_s16 *c, int16_t data);
@@ -34,6 +35,30 @@ int8_t buf_pop_s16(cbuf_s16 *c, int16_t *data);
 int8_t buf_push_u8(cbuf_u8 *c, uint8_t data);
 int8_t buf_pop_u8(cbuf_u8 *c, uint8_t *data);
 
+//New ring buffer realizaion
 
+typedef struct {
+  uint8_t *data;
+  uint32_t size;
+  uint32_t head;
+  uint32_t tail;
+  uint32_t length;
+} rb_u8;
+
+typedef struct {
+  uint16_t *data;
+  uint32_t size;
+  uint32_t head;
+  uint32_t tail;
+  uint32_t length;
+} rb_u16;
+
+
+uint8_t rb_u8_push(rb_u8 *ring, uint8_t data);
+uint8_t rb_u16_push(rb_u16 *ring, uint16_t data);
+uint8_t rb_u8_pop(rb_u8 *ring, uint8_t *data);
+uint8_t rb_u16_pop(rb_u16 *ring, uint16_t *data);
+
+//
 
 #endif
